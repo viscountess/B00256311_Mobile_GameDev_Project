48218a69e870f80cb2d8ff56b4d95eb4149560fb
viscountess <venerable_viscountess@hotmail.com>
2016-05-06 10:10:03 +0100

    Draw Board
    Used bean placeholder images for now.
    
    Used a bitmap object for the squares
    For each cell, a new square and block is created
    Brought a group of the blocks to the front, so that it shows on top of
    the squares

--- Build/js/states/Game.js
+++ Build/js/states/Game.js
@@ -17,12 +17,12 @@ BattleQ.GameState = {
     this.background = this.add.sprite(0, 0, 'background');
     this.blocks = this.add.group();
 
-  //board model
+    //board model
     this.board = new BattleQ.Board(this, this.NUM_ROWS, this.NUM_COLS, this.NUM_VARIATIONS);
     this.board.consoleLog();
 
+    this.drawBoard();
   },
-
   createBlock: function(x, y, data) {
     var block = this.blocks.getFirstExists(false);
 
@@ -34,7 +34,31 @@ BattleQ.GameState = {
       block.reset(x, y, data);
     }
 
-  return block;
-}
+    return block;
+  },
+  drawBoard: function() {
+    var i, j, block, square, x, y, data;
+
+    //semi-transparent coloured squares
+    var squareBitmap = this.add.bitmapData(this.BLOCK_SIZE + 4, this.BLOCK_SIZE + 4);
+    squareBitmap.ctx.fillStyle = '#ff99ff';
+    squareBitmap.ctx.fillRect(0, 0, this.BLOCK_SIZE + 4, this.BLOCK_SIZE + 4);
+
+    for(i = 0; i < this.NUM_ROWS; i++) {
+      for(j = 0; j < this.NUM_COLS; j++) {
+        //Position of the board
+        x = 165 + j * (this.BLOCK_SIZE + 6);
+        y = 150 + i * (this.BLOCK_SIZE + 6);
+
+        square = this.add.sprite(x, y, squareBitmap);
+        square.anchor.setTo(0.5);
+        square.alpha = 0.2;
+
+        this.createBlock(x, y, {asset: 'block' + this.board.grid[i][j], row: i, col: j});
+      }
+    }
+
+    this.game.world.bringToTop(this.blocks);
+  }
 
 };
